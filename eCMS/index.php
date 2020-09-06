832800ea279ad1ad9a6bb3313448d02c0cd8f017
isrt09 <mazedur@gmail.com>
2020-09-07 02:33:34 +0600

    CSS bugs fixed for layout & logo, Banner added

--- eCMS/index.php
+++ eCMS/index.php
@@ -7,8 +7,18 @@
 	<link rel="stylesheet" href="style.css" media="all">
 </head>
 <body>
-	<div id="header">This is the header</div>	
-	<div id="navbar">This is the Navigation</div>	
+	<div id="header">
+		<div id="logo"><img src="images/logo.png" alt=""></div>		
+	</div>	
+	<div id="navbar">
+		<ul id="ul">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Biography</a></li>
+			<li><a href="#">Gallery</a></li>
+			<li><a href="#">Contact</a></li>
+		</ul>
+	</div>	
 	<div id="main_content">This is the Content</div>	
 	<div id="sidebar">This is the Sidebar</div>	
 	<div id="footer">This is the Footer</div>	
