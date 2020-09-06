5f69b3446989c246a63769d229d38cc39ffe4302
isrt09 <mazedur@gmail.com>
2020-09-07 01:43:00 +0600

    HTML/CSS Layout setup is done

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Malala Yousafzai</title>
+	<link rel="stylesheet" href="style.css" media="all">
+</head>
+<body>
+	<div id="header">This is the header</div>	
+	<div id="navbar">This is the Navigation</div>	
+	<div id="main_content">This is the Content</div>	
+	<div id="sidebar">This is the Sidebar</div>	
+	<div id="footer">This is the Footer</div>	
+</body>
+</html>
