3f83a864eaf0b5525d84f277cc66a356c4d16799
isrt09 <mazedur@gmail.com>
2020-09-11 03:07:18 +0600

    News CMS Layout

---
+++ CMS/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Content Management System || CMS</title>
+	<link rel="stylesheet" href="styles/style.css" media="all">
+</head>
+<body>
+	<div class="container">
+		<div class="head">			
+			<img id="logo" src="images/logo.png" alt="" >
+			<img id="banner" src="images/ad_banner.gif" alt="">
+		</div>		
+		<div class="navbar">This is Menu</div>
+		<div class="content">This is Content</div>
+		<div class="sidebar">This is SideBar</div>
+		<div class="footer">This is Footer</div>
+	</div>
+</body>
+</html>
