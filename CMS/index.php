9a8d292f13068dfe7d3930471f5a7dc4cd5c1736
isrt09 <mazedur@gmail.com>
2020-09-12 02:02:11 +0600

    refix CSS home page

--- CMS/index.php
+++ CMS/index.php
@@ -45,19 +45,18 @@
 				$query  = "SELECT * FROM posts ORDER BY rand() LIMIT 0, 5";
 				$result = mysqli_query($con, $query);
 				while($row = mysqli_fetch_assoc($result)){
+					$post_id = $row['post_id'];
 					$title   = $row['post_title'];
 					$author  = $row['post_author'];
 					$keyword = $row['post_keywords'];										
 					$date    = $row['post_date'];										
 					$photo   = $row['post_image'];										
-					$content = $row['post_content'];
+					$content = substr($row['post_content'], 0,300);
 					echo "
-					<p>$title</p>
-					<p>$author</p>
-					<p>$keyword</p>
-					<p>$date</p>					
+					<h2 id='head'>$title</h2><br/>
+					<span><b>Author : </b><i>$author</i></span>,&nbsp;<span><b>Keywords : </b>$keyword</span>,&nbsp;<span><b>Published dated by : </b>$date</span>								
 					<img src='admin/news_images/$photo' width='100' height='100'>
-					<p>$content</p>
+					<div>$content <a href='details.php?id=$post_id' style='float: right;' id='rmlink'>READ MORE</a></div><br/>
 
 
 
@@ -67,8 +66,7 @@
 				}
 			?>				
 		</div>
-		<!-- Main Content End -->
-		
+		<!-- Main Content End -->		
 		<!-- SideBar Start -->
 		<div class="sidebar">
 			<ul id="categories">
